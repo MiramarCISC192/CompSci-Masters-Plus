ba2e2b0e28c2e02b07e92eabf34c7586a584bf2d
Tiqur <55461688+Tiqur@users.noreply.github.com>
2020-12-12 13:33:20 -0800

    Updated main function to follow output format

--- SavingsAccount.cpp
+++ SavingsAccount.cpp
@@ -16,36 +16,30 @@ private:
         cents = cents % 100;
     }
 public:
-    SavingsAccount();
+    SavingsAccount(int, int);
     void deposit();
     void display();
     void withdraw();
 };
 //Constructor for the SavingsAccount class, takes user input for initial dollar and cents values.
 //William Box
-SavingsAccount::SavingsAccount(){
-    cout << "Please input the initial dollars" << endl;
-    cin >> dollars;
-    cout << "Please input the initial cents" << endl;
-    cin >> cents;
+SavingsAccount::SavingsAccount(int inital_dollars, int initial_cents){
+    dollars = inital_dollars;
+    cents = initial_cents;
 }
+
 void SavingsAccount::deposit() {
     // Deposit function. Asks if user wants to deposit. If user inputs right character, changes the "dollars" and "cents" variables accordingly.
     int interim_dollars=0;
     int interim_cents=0;
-    char deposit;
-    cout << "Would you like to make a deposit? Y or y for yes. Enter any other value for skipping this step" << endl;
-    cin >> deposit;
-    if (deposit == 'y' || deposit == 'Y')
-    {
-        cout << "Please input dollars to be deposited" << endl;
-        cin >> interim_dollars;
-        cout << "Please input cents to be deposited" << endl;
-        cin >> interim_cents;
-    }
+
+    cout << "Please input dollars to be deposited" << endl;
+    cin >> interim_dollars;
+    cout << "Please input cents to be deposited" << endl;
+    cin >> interim_cents;
+
     dollars += interim_dollars;
     cents += interim_cents;
-    SavingsAccount::display();
 }
 // Withdraw function, Asks if user wants to withdraw money. If they do, the function updates the "dollars" and "cents" values accordingly.
 // Written by William Box using the deposit function from Auityan as reference.
@@ -53,18 +47,14 @@ void SavingsAccount::withdraw() {
     int interim_dollars=0;
     int interim_cents=0;
     char withdraw;
-    cout << "Would you like to make a withdraw? Y or y for yes. Enter any other value for skipping this step" << endl;
-    cin >> withdraw;
-    if (withdraw == 'y' || withdraw == 'Y')
-    {
-        cout << "Please input dollars to be withdrawn" << endl;
-        cin >> interim_dollars;
-        cout << "Please input cents to be withdrawn" << endl;
-        cin >> interim_cents;
-    }
+
+    cout << "Please input dollars to be withdrawn" << endl;
+    cin >> interim_dollars;
+    cout << "Please input cents to be withdrawn" << endl;
+    cin >> interim_cents;
+
     dollars -= interim_dollars;
     cents -= interim_cents;
-    SavingsAccount::display();
 }
 //takes no parameters and returns nothing, simply prints out the current amount of dollars and cents after calling normalize()
 //Austin Folster
@@ -76,11 +66,31 @@ void SavingsAccount::display() {
 // main program / loop
 // Trevor Brage
 int main() {
-    SavingsAccount savings_account = SavingsAccount();
-    savings_account.display();
+    int initial_dollars = 0;
+    int initial_cents = 0;
+    char c;
+
+    cout << "Please input the initial dollars" << endl;
+    cin >> initial_dollars;
+    cout << "Please input the initial cents" << endl;
+    cin >> initial_cents;
+
+    SavingsAccount savings_account = SavingsAccount(initial_dollars, initial_cents);
 
     while (true) {
-        savings_account.deposit();
-        savings_account.withdraw();
+        cout << "Would you like to make a deposit? Y or y for yes." << endl;
+        cin >> c;
+        if (c == 'y' || c == 'Y')
+            savings_account.deposit();
+        else  if (c == 'n' || c == 'N') break;
     }
+
+    while (true) {
+        cout << "Would you like to make a withdrawal? Y or y for yes." << endl;
+        cin >> c;
+        if (c == 'y' || c == 'Y')
+            savings_account.withdraw();
+        else  if (c == 'n' || c == 'N') break;
+    }
+    savings_account.display();
 }
